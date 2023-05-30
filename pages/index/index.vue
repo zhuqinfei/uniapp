e31ce46079da58549bbfbec50fe33819e6b3277d
zhuqinfei <1586859762@qq.com>
2023-05-30 17:56:25 +0800

    创建项目和导航开发

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="index">
+		<view class="f-active-color">文字</view>
+		<view class='iconfont icon-xiaoxi'>出来了</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
