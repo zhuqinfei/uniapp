bd7534e11eaa140786614417413b3d3a87555db2
zhuqinfei <1586859762@qq.com>
2023-06-12 16:15:02 +0800

    商品分类内容布局

--- pages/list/list.vue
+++ pages/list/list.vue
@@ -1,6 +1,67 @@
 <template>
-	<view class="list">
+	<view>
 		<Lines></Lines>
+		
+		<view class="list">
+			<!-- 左侧滑动 -->
+			<scroll-view scroll-y="true" class="list-left">
+				<view v-for="i in 50" class="left-item">
+					<view class="left-name">{{i}}</view>
+				</view>
+			</scroll-view>
+			<!-- 右侧滑动 -->
+			<scroll-view scroll-y="true" class="list-right">
+				<view v-for="i in 50" class="right-item">
+					<view class="right-list">
+						<view class="list-title">家纺</view>
+						<view class="right-content">
+							<view class="right-item">
+								<image class="right-img" src="../../static/img/list1.jpg" mode=""></image>
+								<view class="right-name">毛巾</view>
+							</view>
+							<view class="right-item">
+								<image class="right-img" src="../../static/img/list1.jpg" mode=""></image>
+								<view class="right-name">毛巾</view>
+							</view>
+							<view class="right-item">
+								<image class="right-img" src="../../static/img/list1.jpg" mode=""></image>
+								<view class="right-name">毛巾</view>
+							</view>
+							<view class="right-item">
+								<image class="right-img" src="../../static/img/list1.jpg" mode=""></image>
+								<view class="right-name">毛巾</view>
+							</view>
+						</view>
+					</view>
+					<view class="right-list">
+						<view class="list-title">家纺</view>
+						<view class="right-content">
+							<view class="right-item">
+								<image class="right-img" src="../../static/img/list1.jpg" mode=""></image>
+								<view class="right-name">毛巾</view>
+							</view>
+							<view class="right-item">
+								<image class="right-img" src="../../static/img/list1.jpg" mode=""></image>
+								<view class="right-name">毛巾</view>
+							</view>
+							<view class="right-item">
+								<image class="right-img" src="../../static/img/list1.jpg" mode=""></image>
+								<view class="right-name">毛巾</view>
+							</view>
+						</view>
+					</view>
+					<view class="right-list">
+						<view class="list-title">家纺</view>
+						<view class="right-content">
+							<view class="right-item">
+								<image class="right-img" src="../../static/img/list1.jpg" mode=""></image>
+								<view class="right-name">毛巾</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</scroll-view>
+		</view>
 		
 	</view>
 </template>
@@ -28,6 +89,52 @@
 	}
 </script>
 
-<style>
-
+<style scoped>
+.list{
+	display:flex;
+}
+.list-left{
+	width:200rpx;
+}
+.left-item{
+	border-bottom :2rpx solid #FFFFFF;
+	font-size:28rpx;
+	font-weight: bold;
+	background-color: #F7F7F7;
+}
+.left-name{
+	padding:30rpx 6rpx;
+	text-align: center;
+}
+.left-name-active{
+	border-left:8rpx solid #49BDFb;
+	background-color: #FFFFFF;
+}
+.list-right{
+	flex:1;
+	padding-left: 20rpx;
+}
+.list-title{
+	font-weight: bold;
+	padding:30rpx 0;
+}
+.right-content{
+	display: flex;
+	flex-wrap: wrap;
+}
+.right-item{
+	widhth:150rpx;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding:10rpx;
+}
+.right-name{
+	padding:16rpx 0;
+}
+.right-img{
+	width:150rpx;
+	height: 150rpx;
+}
 </style>
