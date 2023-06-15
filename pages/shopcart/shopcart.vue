6e299a1cc73a3917f2bd62bf5654e34d4e0ef309
zhuqinfei <1586859762@qq.com>
2023-06-15 18:14:20 +0800

    【购物车】内容块布局

--- pages/shopcart/shopcart.vue
+++ pages/shopcart/shopcart.vue
@@ -8,6 +8,35 @@
 		statusBar="true"
 		@clickRight="isNavBar=! isNavBar"
 		></uniNavBar>
+		
+		<!-- 商品内容 -->
+		<view class="shop-list">
+			<view class="shop-item" v-for="(item,index) in list" :key="index">
+				<label class="radio">
+					<radio value="" color="#FF3333"/><text></text>
+				</label>
+				<image class="shop-img" :src="item.imgUrl" mode=""></image>
+				<view class="shop-text">
+					<view class="shop-name">{{item.name}}</view>
+					<view class="shop-color f-color">{{item.color}}</view>
+					<view class="shop-price">
+						<view>￥{{item.pprice}}</view>
+						<view>*{{item.num}}</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+		<!-- 底部 -->
+		<view class='shop-foot'>
+			<label class="radio foot-radio">
+				<radio value="" color='#FF3333'/><text>全选</text>
+			</label>
+			<view class='foot-total'>
+				<view class='foot-count'>合计：<text class='f-active-color'>¥0</text></view>
+				<view class='foot-num'>结算(0)</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -16,7 +45,72 @@
 	export default {
 		data() {
 			return {
-				isNavBar:false
+				isNavBar:false,
+				list:[
+					{
+						checked:false,
+						id:1,
+						name:"打客服的和非恶搞认为微软业务打客服的和非恶搞认为微软业务",
+						color:"颜色：返回对方的过分的",
+						imgUrl:"../../static/img/Children2.jpg",
+						pprice:"27",
+						num:1
+					},
+					{
+						checked:false,
+						id:2,
+						name:"打客服的和非恶搞认为微软业务打客服的和非恶搞认为微软业务",
+						color:"颜色：返回对方的过分的",
+						imgUrl:"../../static/img/Children2.jpg",
+						pprice:"27",
+						num:1
+					},
+					{
+						checked:false,
+						id:2,
+						name:"打客服的和非恶搞认为微软业务打客服的和非恶搞认为微软业务",
+						color:"颜色：返回对方的过分的",
+						imgUrl:"../../static/img/Children2.jpg",
+						pprice:"27",
+						num:1
+					},
+					{
+						checked:false,
+						id:2,
+						name:"打客服的和非恶搞认为微软业务打客服的和非恶搞认为微软业务",
+						color:"颜色：返回对方的过分的",
+						imgUrl:"../../static/img/Children2.jpg",
+						pprice:"27",
+						num:1
+					},
+					{
+						checked:false,
+						id:2,
+						name:"打客服的和非恶搞认为微软业务打客服的和非恶搞认为微软业务",
+						color:"颜色：返回对方的过分的",
+						imgUrl:"../../static/img/Children2.jpg",
+						pprice:"27",
+						num:1
+					},
+					{
+						checked:false,
+						id:2,
+						name:"打客服的和非恶搞认为微软业务打客服的和非恶搞认为微软业务",
+						color:"颜色：返回对方的过分的",
+						imgUrl:"../../static/img/Children2.jpg",
+						pprice:"27",
+						num:1
+					},
+					{
+						checked:false,
+						id:2,
+						name:"打客服的和非恶搞认为微软业务打客服的和非恶搞认为微软业务",
+						color:"颜色：返回对方的过分的",
+						imgUrl:"../../static/img/Children2.jpg",
+						pprice:"27",
+						num:1
+					}
+				]
 			}
 		},
 		components:{
@@ -29,5 +123,59 @@
 </script>
 
 <style>
+.shop-list{
+	padding-bottom:100rpx;
+}
+.shop-item{
+	display: flex;
+	padding:20rpx;
+	align-items: center;
+	background-color: #F7F7F7;
+	margin-bottom: 10rpx;
+}
+.shop-img{
+	width:200rpx;
+	height: 200rpx;
+}
+.shop-text{
+	flex:1;
+	padding-left:20rpx;
+}
+.shop-color{
+	font-size: 20rpx;
+}
+.shop-price{
+	display: flex;
+	justify-content: space-between;
+}
+.shop-foot{
+    position: fixed;
+	bottom: var(--window-bottom);
+	left:0;
+	width: 100%;
+	height: 100rpx;
+	background-color: #FFFFFF;
+	border-top: 2rpx solid #F7F7F7;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 
+}
+.foot-radio{
+	padding-left:20rpx;
+}
+.foot-total{
+	display: flex;
+}
+.foot-count{
+	line-height: 100rpx;
+	padding:0 20rpx;
+	font-size:32rpx;
+}
+.foot-num{
+	background-color: #49BDFB;
+	color:#FFFFFF;
+	padding:0 60rpx;
+	line-height: 100rpx;
+}
 </style>
