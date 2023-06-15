5043c2168385315db3d5158d432e3565fdf842a9
zhuqinfei <1586859762@qq.com>
2023-06-15 16:33:14 +0800

    自定义导航栏

--- pages/shopcart/shopcart.vue
+++ pages/shopcart/shopcart.vue
@@ -1,15 +1,26 @@
 <template>
-	<view>
-		
+	<view class="shop-cart">
+		<!-- 自定义导航栏 -->
+		<uniNavBar 
+		title="购物车" 
+		:rightText="isNavBar ? '完成' : '编辑' " 
+		fixed="true"
+		statusBar="true"
+		@clickRight="isNavBar=! isNavBar"
+		></uniNavBar>
 	</view>
 </template>
 
 <script>
+	import uniNavBar from "../../components/uni/uni-nav-bar/uni-nav-bar.vue"
 	export default {
 		data() {
 			return {
-				
+				isNavBar:false
 			}
+		},
+		components:{
+			uniNavBar
 		},
 		methods: {
 			
